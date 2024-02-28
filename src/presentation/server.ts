f034c0f73d15ae3f67167af27d259aeed99dfe45
gustavo <gustavo.perez.231191@gmail.com>
2024-02-27 22:57:15 -0600

    Deploy new server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,29 +1,38 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
+    routes: Router;
     public_path?: string;
 }
 
 export class Server {
     private app = express();
-    private port: number;
-    private publicPath: string;
+    private readonly port: number;
+    private readonly publicPath: string;
+    private readonly routes: Router;
 
     constructor(options: Options) {
-        const { port, public_path = 'public' } = options;
+        const { port, routes, public_path = 'public' } = options;
 
         this.port = port;
         this.publicPath = public_path;
+        this.routes = routes;
     }
 
     async start() {
-        //Middlewares
+        //* Middlewares
+        this.app.use(express.json()); // raw
+        this.app.use(express.urlencoded({ extended: true })); // x-www-formurlencoded
 
-        //public folder
+        //* Public folder
         this.app.use(express.static(this.publicPath));
 
+        //* Routes
+        this.app.use(this.routes);
+
+        //* SPA
         this.app.get('*', (req, res) => {
             const indexPath = path.join(
                 __dirname + `../../../${this.publicPath}/index.html`
